9719760537f2f1decb47f3fa88468112dd2a2750
jaco4873 <124041503+jaco4873@users.noreply.github.com>
2024-08-01 13:08:53 +0200

    updated build script

--- part11/patientor-with-pipeline/build_step.sh
+++ part11/patientor-with-pipeline/build_step.sh
@@ -2,6 +2,17 @@
 
 echo "Build script"
 
+# Navigate to the client directory
+cd client
+npm install
+npm run lint
+npm run build
+
+# Navigate back to the root directory
+cd ..
+
+# Navigate to the server directory
+cd server
 npm install
 npm run lint
 npm run build
