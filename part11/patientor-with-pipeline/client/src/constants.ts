929bd94419d8d641ae23c5f332d9c5a8e801ffdc
jaco4873 <124041503+jaco4873@users.noreply.github.com>
2024-08-01 13:43:30 +0200

    updated baseUrl to work in prod env

--- part11/patientor-with-pipeline/client/src/constants.ts
+++ part11/patientor-with-pipeline/client/src/constants.ts
@@ -1,1 +1,7 @@
-export const apiBaseUrl = "http://localhost:3001/api";
+const env = import.meta.env.VITE_ENV;
+const productionApiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+
+// Set the apiBaseUrl based on the environment
+export const apiBaseUrl = env === 'dev'
+  ? 'http://localhost:3001/api'
+  : productionApiBaseUrl;
